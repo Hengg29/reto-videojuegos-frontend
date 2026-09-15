18df1cc195ddfd69948014474897031fa318cb7f
Alexander Hernandez Henggeler <alexander.hernandez@iest.edu.mx>
2026-09-15 11:21:10 -0600

    instalacion de tailwind listo para empezar el frontend

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server: {
     // Cualquier petición a /api/* desde el frontend se redirige
     // automáticamente al backend (http://localhost:4000).
